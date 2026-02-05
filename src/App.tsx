4bea7b19c96b7cd4f8d0161913ee3fd3a8d13287
noahsafar <noah.safar@yale.edu>
2026-02-05 09:52:08 -0500

    Further lighten Compose button hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -640,7 +640,7 @@ function App() {
                     variant="outline"
                     size="sm"
                     onClick={handleCompose}
-                    className="flex-shrink-0 hover:bg-gray-100 dark:hover:bg-gray-800"
+                    className="flex-shrink-0 hover:bg-gray-50 dark:hover:bg-gray-900"
                   >
                     <PenSquare className="h-4 w-4 mr-1.5" />
                     Compose
